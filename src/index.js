7ad74ced0ff440e361a559463815de1e4a55b3ab
Timmiyar <timmiyar@gmail.com>
2025-12-01 11:36:43 -0600

    Allow '/wisp' without trailing slash in routing

--- src/index.js
+++ src/index.js
@@ -28,8 +28,11 @@ const fastify = Fastify({
 				handler(req, res);
 			})
 			.on("upgrade", (req, socket, head) => {
-				if (req.url.endsWith("/wisp/")) wisp.routeRequest(req, socket, head);
-				else socket.end();
+				if (req.url.endsWith("/wisp/") || req.url.endsWith("/wisp")) {
+					wisp.routeRequest(req, socket, head);
+				} else {
+					socket.end();
+				}
 			});
 	},
 });
